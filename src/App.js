238710f7f0b0e1e7b2d4b41bbd335d988ed0c67b
ahmadabdolsaheb <ahmad.abdolsaheb@gmail.com>
2018-01-17 12:04:13 +0300

    responsive image

--- src/App.js
+++ src/App.js
@@ -1,170 +1,78 @@
 import React, { Component } from 'react';
 import './App.css';
 import StackGrid from "react-stack-grid";
+import cards from './data/cards';
+const tuba = require('./assets/tuba.jpg');
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      cards: cards
+    };
+    this.showAll = this.showAll.bind(this);
+    this.showCards = this.showCards.bind(this);
+  }
+  showAll(){
+    this.setState({
+        cards: cards
+      });
+  }
+
+  showCards(type){
+    this.setState({
+        cards: cards.filter(card => card.type === type)
+      });
+  }
+
+
   render() {
+    console.log(this.state.cards);
+
     return (
       <div className="App">
+          <div className="holder">
+          <img class="img-fluid" src="http://via.placeholder.com/1080x600"/>
+          <div className="cover container">
+            <div className="row">
+              <h1 className="col-md-4 offset-md-8 card-title">TUBA <br/> YILMAZ</h1>
+            </div>
+            <div className="row">
+            <p className="col-md-4 offset-md-8 card-text">
+              Dr. Tuba Yilmaz Abdolsaheb is an Assistant Professor and a Marie Sklodowska Curie
+              Research Fellow in Department of Electronics and Communication Engineering
+              at Istanbul Technical University (ITU).</p>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row align-items-center justify-content-md-center">
+            <div className = "buttons col col-md-7">
+              <a onClick={this.showAll} className="btn btn-primary">all</a>
+              <a onClick={() => this.showCards("projects")} className="btn btn-primary">projects</a>
+              <a onClick={() => this.showCards("research")} className="btn btn-primary">research</a>
+              <a onClick={() => this.showCards("publications")} className="btn btn-primary">publications</a>
+              <a onClick={() => this.showCards("awards")} className="btn btn-primary">awards</a>
+              <a onClick={() => this.showCards("media")} className="btn btn-primary">media</a>
+            </div>
+        </div>
+      </div>
         <StackGrid className="Stack" columnWidth={300}
           monitorImagesLoaded={true}
           gutterHeight={20}
           gutterWidth={20}
           >
-          <div className="card">
-            <img className="card-img-top" src="../assets/tuba.jpg" alt="Card image cap"/>
-          </div>
-          {/*BIO*/}
-          <div className="card bio" >
-            <div className="card-block">
-              <h1 className="card-title">TUBA YILMAZ</h1>
-              <p className="card-text">
-                Dr. Tuba Yilmaz Abdolsaheb is an Assistant Professor and a Marie Sklodowska Curie
-                Research Fellow in Department of Electronics and Communication Engineering
-                at Istanbul Technical University (ITU).</p>
-            </div>
-          </div>
-          {/*PROJECTS*/}
-          <div className="card projects">
-            <div className="card-block">
-                <h2 className="card-title">PROJECTS</h2>
-            </div>
-          </div>
-          <div className="card projects">
-            <div className="card-block">
-              <h5 className="card-title">H2020 - MARIE SKLODOWSKA CURIE INDIVIDUAL FELLOWSHIP - RI Grant</h5>
-              <p className="card-text">Principal Investigator, microwave Diagnosis Of Breast Cancer With Open Ended Contact Probe - MIDxPRO, Budget: 157,845 EUR, 2017-2019.</p>
-            </div>
-          </div>
-          <div className="card projects">
-            <div className="card-block">
-              <h5 className="card-title">TUBITAK 1003</h5>
-              <p className="card-text">Researcher, Meme Kanserinin Tanısı Için Magnetik-nanoparçacık Temelli Mikrodalga Görüntüleme Teknolojisinin Geliştirilmesi,  Budget: 1,500,000 TL, 2017-2019.</p>
-            </div>
-          </div>
-          <div className="card projects">
-            <div className="card-block">
-              <h5 className="card-title">KALKINMA BAKANLIĞI</h5>
-              <p className="card-text">Researcher, Ambulanslar Için Beyin Kanaması Erken Teşhis Ve Tespit Sistemi:kask, Budget: 875,312 TL, 2016 - 2017</p>
-            </div>
-          </div>
-
-          {/*RESEARCH*/}
-          <div className="card research">
-            <div className="card-block">
-                <h2 className="card-title">RESEARCH</h2>
-            </div>
-          </div>
-          <div className="card research">
-            <div className="card-block">
-              <h4 className="card-title">Dielectric Spectroscopy</h4>
-            </div>
-          </div>
-          <div className="card research">
-            <div className="card-block">
-              <h4 className="card-title">Design and Testing of
-Microwave Wearable Sensors</h4>
-            </div>
-          </div>
-          <div className="card research">
-            <div className="card-block">
-              <h4 className="card-title">Phantom Characterization for
-bio-electromagnetics
-applications</h4>
-            </div>
-          </div>
-          <div className="card research">
-            <div className="card-block">
-              <h4 className="card-title">Design and Testing of
-On-body Antennas</h4>
-            </div>
-          </div>
-          <div className="card research">
-            <div className="card-block">
-              <h4 className="card-title">Design and Testing of
-Implantable Antennas</h4>
-            </div>
-          </div>
-          <div className="card research">
-            <div className="card-block">
-              <h4 className="card-title .no_margin">Future Work: AI implemented
-sensing platforms</h4>
-            </div>
-          </div>
-          {/*PUBLICATION*/}
-          <div className="card publication">
-            <div className="card-block">
-                <h2 className="card-title">PUBLICATION</h2>
-            </div>
-          </div>
-          <div className="card publication">
-            <div className="card-block">
-              <h6 className="card-text">T Yilmaz, T Ozturk, S Joof. A Comparative Study for Development of Microwave Glucose Sensors</h6>
-            </div>
-          </div>
-          <div className="card publication">
-            <div className="card-block">
-              <h6 className="card-text">T Yilmaz, N Hasan, R Zane, Z Pantic. Multi-Objective Optimization of Circular Magnetic Couplers for Wireless Power Transfer Applications</h6>
-            </div>
-          </div>
-          <div className="card publication">
-            <div className="card-block">
-              <h6 className="card-text">T Yilmaz, H Sahinturk, M Acar, I Akduman. Matching medium characterization for microwave brain stroke imaging</h6>
-            </div>
-          </div>
-          <div className="card publication">
-            <div className="card-block">
-              <h6 className="card-text">K Cakmak, T Ozturk, T Yilmaz, I Akduman. Dielectric property measurements of dextrose solutions for RF sensor design</h6>
-            </div>
-          </div>
-          <div className="card publication">
-            <div className="card-block">
-              <h6 className="card-text">T Yilmaz, MA Kılıç, M Erdoğan, M Çayören, D Tunaoğlu, İ Kurtoğlu, ... Machine learning aided diagnosis of hepatic malignancies through in vivo dielectric measurements with microwaves</h6>
-            </div>
-          </div>
-          <div className="card more">
-            <a href="https://scholar.google.com/citations?hl=en&user=U2nbt_IAAAAJ&view_op=list_works&sortby=pubdate" className="btn btn-primary">MORE PUBLICATIONS</a>
-          </div>
-          {/*AWARDS*/}
-          <div className="card awards">
-            <div className="card-block">
-                <h2 className="card-title">AWARDS</h2>
-            </div>
-          </div>
-
-          <div className="card awards">
-            <div className="card-block">
-              <h5 className="card-title">URSI Young Scientist Award, 2017.</h5>
-            </div>
-          </div>
-          <div className="card awards">
-            <div className="card-block">
-              <h5 className="card-title">Marie Curie Individual Fellowship – Reintegration Grant, 2016.</h5>
-            </div>
-          </div>
-          <div className="card awards">
-            <div className="card-block">
-              <h5 className="card-title">IET Today’s RF Tomorrow’s Medicine Workshop 2nd runner up prize, 2016.</h5>
-            </div>
-          </div>
-          {/*MEDIA*/}
-          <div className="card media">
-            <div className="card-block">
-                <h2 className="card-title">MEDIA</h2>
-                  <div className="card-block">
-                    <a href="http://www.itu.edu.tr/haberler/2017/02/22/uc-projemiz-marie-sklodowska-curie-bireysel-arastirma-burslari-2016-destegi-alacak" className="btn btn-primary">ITU Haberler</a>
-                      <a href="https://www.ituvakif.org.tr/dergi/sayi_76.pdf" className="btn btn-primary">ITU Vakif</a>
-
-                  </div>
-            </div>
-          </div>
-          {/*MORE*/}
-          <div className="card more">
-            <a href="#" className="btn btn-primary">DOWNLOAD CV</a>
-          </div>
-          <div className="card more">
-            <a href="#" className="btn btn-primary">SEND EMAIL</a>
-          </div>
+          {this.state.cards.map((card, index) =>
+              <div className="card" key={index}>
+                <div className="card-block">
+                  <span className="badge badge-secondary">{card.type}</span>
+                  {card.title ? <h4 className="card-title">{card.title}</h4>:''}
+                  {card.content ? <p className="card-title">{card.content}</p>:''}
+                  {card.link? <a href={card.link} className="btn btn-primary">link</a>:''}
+                </div>
+              </div>
+          )}
           </StackGrid>
       </div>
     );
---
+++ src/data/cards.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    "type":"projects",
+    "title":"H2020 - MARIE SKLODOWSKA CURIE INDIVIDUAL FELLOWSHIP - RI Grant",
+    "content": "Principal Investigator, microwave Diagnosis Of Breast Cancer With Open Ended Contact Probe - MIDxPRO, Budget: 157,845 EUR, 2017-2019."
+  },
+  {
+    "type":"projects",
+    "title":"TUBITAK 1003",
+    "content": "Researcher, Meme Kanserinin Tanısı Için Magnetik-nanoparçacık Temelli Mikrodalga Görüntüleme Teknolojisinin Geliştirilmesi,  Budget: 1,500,000 TL, 2017-2019."
+  },
+  {
+    "type":"projects",
+    "title":"ISTANBUL DEVELOPMENT AGENCY",
+    "content": "Researcher, Ambulanslar Için Beyin Kanaması Erken Teşhis Ve Tespit Sistemi:kask, Budget: 875,312 TL, 2016 - 2017"
+  },
+  {
+    "type":"research",
+    "title":"Dielectric Spectroscopy",
+    "content": ""
+  },
+  {
+    "type":"research",
+    "title":"Design and Testing of Microwave Wearable Sensors",
+    "content": ""
+  },
+  {
+    "type":"research",
+    "title":"Phantom Characterization for bio-electromagnetics applications",
+    "content": ""
+  },
+  {
+    "type":"research",
+    "title":"Design and Testing of On-body Antennas",
+    "content": ""
+  },
+  {
+    "type":"research",
+    "title":"Design and Testing of Implantable Antennas",
+    "content": ""
+  },
+  {
+    "type":"publications",
+    "title":"",
+    "content": "T Yilmaz, N Hasan, R Zane, Z Pantic. Multi-Objective Optimization of Circular Magnetic Couplers for Wireless Power Transfer Applications"
+  },
+  {
+    "type":"awards",
+    "title":"URSI Young Scientist Award, 2017.",
+    "content": ""
+  },
+  {
+    "type":"awards",
+    "title":"Marie Curie Individual Fellowship – Reintegration Grant, 2016.",
+    "content": ""
+  },
+  {
+    "type":"awards",
+    "title":"IET Today’s RF Tomorrow’s Medicine Workshop 2nd runner up prize, 2016.",
+    "content": ""
+  },
+]
